e844209e37fe0d78c139c00727b28f1c99226bfd
Xaofly Sho <love-shao@qq.com>
2016-09-01 19:10:58 +0800

    ADD Help Document

--- XSString/NSString+XSString.h
+++ XSString/NSString+XSString.h
@@ -10,18 +10,28 @@
 
 @interface NSString (XSString)
 
-/*
+/*!
  移除字符串中的HTML标签
+ 
+ @return NSString 移除HTML标签后的字符串
  */
 - (NSString *)stringByRemovingHTMLMark;
 
-/*
+/*!
  生成一个由大写英文字母组成的随机字符串
+ 
+ @param lenght 字符串长度
+ 
+ @return NSString 生成的相应长度的字符串
  */
 + (NSString *)generatesRandomStringWithLength:(int)lenght;
 
-/*
+/*!
  将JSON对象（NSArray、NSDictionary）转换为字符串
+ 
+ @param JSONObject JSON对象（NSArray、NSDictionary）
+ 
+ @return NSString 转换后的字符串
  */
 + (NSString *)stringByJSONObject:(id)JSONObject;
 
